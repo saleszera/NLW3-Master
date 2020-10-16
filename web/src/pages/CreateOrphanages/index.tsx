1ed4c6c578d4d42244d46f79dfbd420b4a4b0814
Thiago Alves <the-one-who-knoccks@pop-os.localdomain>
2020-10-15 21:25:48 -0300

    :fire: Front-end application

--- web/src/pages/CreateOrphanages/index.tsx
+++ web/src/pages/CreateOrphanages/index.tsx
@@ -86,13 +86,13 @@ export default function CreateOrphanage() {
             <legend>Dados</legend>
 
             <Map
-              center={[-27.2092052, -49.6401092]}
+              center={[-21.7713191, -42.5404258]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
               onclick={handleMapClick}
             >
               <TileLayer
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
               {position.latitude !== 0 && (
                 <Marker
